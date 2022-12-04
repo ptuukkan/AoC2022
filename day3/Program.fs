d568227799a1c1d31bd7cf7231f73c0d9f073ec9
Pekka Tuukkanen <petuukka@gmail.com>
2022-12-04 20:32:26 +0100

    Day 3 and 4

---
+++ day3/Program.fs
@@ -0,0 +1,60 @@
+﻿open System.IO
+open System
+
+let halfString str =
+    let half = Operators.(/) (String.length str) 2
+    (str.Substring(0, half), str.Substring(half))
+
+let intersect (str1: string, str2: string) =
+    Set.ofSeq str1
+    |> Set.intersect (Set.ofSeq str2)
+    |> Set.toList
+
+let charToScore c =
+    if Char.IsUpper c
+    then int c - 38
+    else int c - 96
+
+let intersectChunk (chunk: list<string>) =
+    Set.ofSeq chunk[0]
+    |> Set.intersect (Set.ofSeq chunk[1])
+    |> Set.intersect (Set.ofSeq chunk[2])
+    |> Set.toList
+
+
+[<EntryPoint>]
+let main args =
+    let input = File.ReadLines(args[0])
+    // let input =
+    //     seq {
+    //         "vJrwpWtwJgWrhcsFMMfFFhFp"
+    //         "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+    //         "PmmdzqPrVvPwwTWBwg"
+    //         "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+    //         "ttgJtRGJQctTZtZT"
+    //         "CrZsJsPPZsGzwwsLwLmpwMDw"
+    //     }
+
+    let part1 =
+        input
+        |> List.ofSeq
+        |> List.map halfString
+        |> List.map intersect
+        |> List.map (List.map charToScore)
+        |> List.map List.sum
+        |> List.sum
+
+    let part2 =
+        input
+        |> List.ofSeq
+        |> List.chunkBySize 3
+        |> List.map intersectChunk
+        |> List.map (List.map charToScore)
+        |> List.map List.sum
+        |> List.sum
+
+    printfn "%d" part1
+    printfn "%d" part2
+    0
+
+
---
+++ day4/Program.fs
@@ -0,0 +1,40 @@
+﻿open System.IO
+
+let isOverlap (arr: string[]) =
+    let elf1, elf2 =
+        arr
+        |> Array.map int
+        |> Array.splitAt 2
+    (elf1[0] >= elf2[0] && elf1[1] <= elf2[1]) || (elf1[0] <= elf2[0] && elf1[1] >= elf2[1])
+
+let isPartialOverlap (arr: string[]) =
+    let elf1, elf2 =
+        arr
+        |> Array.map int
+        |> Array.splitAt 2
+    Set.intersect (Set.ofArray [|elf1[0]..elf1[1]|]) (Set.ofArray [|elf2[0]..elf2[1]|])
+    |> (Set.isEmpty >> not)
+
+
+
+[<EntryPoint>]
+let main args =
+    let input = File.ReadLines(args[0])
+    let part1 =
+        input
+        |> List.ofSeq
+        |> List.map (fun x -> x.Split[|','; '-'|])
+        |> List.filter isOverlap
+        |> List.length
+
+    printfn "%d" part1
+
+    let part2 =
+        input
+        |> List.ofSeq
+        |> List.map (fun x -> x.Split[|','; '-'|])
+        |> List.filter isPartialOverlap
+        |> List.length
+
+    printfn "%d" part2
+    0
