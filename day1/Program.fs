9fe74d29ab55094b369d774178798859b027cdfa
Pekka Tuukkanen <petuukka@gmail.com>
2022-12-05 10:33:32 +0100

    Add day 1 and 2

---
+++ day1/Program.fs
@@ -0,0 +1,44 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System.IO
+
+let arraySplit arr = seq {
+    let mutable a = 0
+    let length = Array.length arr - 1
+    for i in 0 .. length do
+        if arr[i] = ""
+        then
+            yield arr[a..i - 1]
+            a <- i + 1
+    if a < length
+    then yield arr[a..length]
+}
+
+[<EntryPoint>]
+let main args =
+    let input = File.ReadLines(args[0])
+    let part1 = 
+        input
+        |> Array.ofSeq
+        |> arraySplit
+        |> Array.ofSeq
+        |> Array.map (fun x -> x |> Array.map int |> Array.sum)
+        |> Array.sortDescending
+        |> Array.head
+        |> string
+        
+    let part2 = 
+        input
+        |> Array.ofSeq
+        |> arraySplit
+        |> Array.ofSeq
+        |> Array.map (fun x -> x |> Array.map int |> Array.sum)
+        |> Array.sortDescending
+        |> Array.take 3
+        |> Array.sum
+        |> string
+    
+    printfn $"Part1: %s{part1}"
+    printfn $"Part2: %s{part2}"
+    0
+    
---
+++ day2/Program.fs
@@ -0,0 +1,81 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System.IO
+
+let isRock char =
+    match char with
+    | "A"
+    | "X" -> true
+    | _ -> false
+
+let isPaper char =
+    match char with
+    | "B"
+    | "Y" -> true
+    | _ -> false
+
+let isScissors char =
+    match char with
+    | "C"
+    | "Z" -> true
+    | _ -> false
+
+let isWin a b =
+    match b with
+    | x when isRock b && isScissors a -> true
+    | y when isPaper b && isRock a -> true
+    | z when isScissors b && isPaper a -> true
+    | _ -> false
+
+let typeScore x =
+    match x with
+    | a when isRock x -> 1
+    | b when isPaper x -> 2
+    | c when isScissors x -> 3
+    | _ -> 0
+
+let play (rps: string []) : int =
+    let a = rps[0]
+    let b = rps[1]
+
+    match b with
+    | win when isWin a b -> 6 + typeScore b
+    | loss when isWin b a -> 0 + typeScore b
+    | _ -> 3 + typeScore b
+
+let deriveSign (rps: string []) : string =
+    let a = rps[0]
+    let b = rps[1]
+
+    match b with
+    | "X" ->
+        [| "A"; "B"; "C" |]
+        |> Array.find (fun x -> isWin x a)
+    | "Z" ->
+        [| "A"; "B"; "C" |]
+        |> Array.find (isWin a)
+    | _ -> a
+
+[<EntryPoint>]
+let main args =
+    let input = File.ReadLines(args[0])
+
+    let part1 =
+        input
+        |> Array.ofSeq
+        |> Array.map (fun x -> x.Split(" ") |> play)
+        |> Array.sum
+
+    printfn $"Part1: %d{part1}"
+
+    let part2 =
+        input
+        |> Array.ofSeq
+        |> Array.map (fun x ->
+            x.Split(" ")
+            |> fun arr -> [| arr[0]; deriveSign arr |] |> play)
+        |> Array.sum
+    
+    printfn $"Part2: %d{part2}"
+
+    0
