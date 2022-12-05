7ae21131900f9cfd2c9ffc512a2540e87a182009
Pekka Tuukkanen <petuukka@gmail.com>
2022-12-05 10:32:44 +0100

    Day5 finished

--- day5/Program.fs
+++ day5/Program.fs
@@ -1,26 +1,39 @@
-﻿open System.IO
+﻿open System.Collections.Generic
+open System.IO
 open System.Text.RegularExpressions
 
 let splitInput input =
     let index = input |> List.findIndex (fun elem -> elem = "")
     input |> List.splitAt index
 
-let createStacks input =
-    let initialValue = [[]; []; []]
-    let foldFunction stacks (elem: string) =
-        let stack1 = stacks[0] @ [elem[1]]
-        let stack2 = stacks[1] @ [elem[5]]
-        let stack3 = stacks[2] @ [elem[9]]
-        [stack1, stack2, stack3]
-    List.fold foldFunction initialValue input
+let createStacks (input: string list) =
+    let stackRow = input |> List.head
+    let stacksCount = stackRow.Substring(stackRow.Length - 2, 1) |> int
+    let initialValue = [1..stacksCount] |> List.map (fun x -> Stack<char>())
+    let foldFunction (stacks: Stack<char> list) (elem: string) =
+        let push (stack: Stack<char>) e =
+            if e <> ' ' then stack.Push(e)
+        [0..stacksCount - 1]
+        |> List.iter (fun x -> push stacks[x] elem[x * 4 + 1])
+        stacks
+    input
+    |> List.tail
+    |> List.fold foldFunction initialValue
 
-let removeEmptyElements (stacks: char list * char list * char list) =
-    let stack1, stack2, stack3 = stacks
-    let newStack1 = List.filter (fun x -> x <> ' ') stack1
-    let newStack2 = List.filter (fun x -> x <> ' ') stack2
-    let newStack3 = List.filter (fun x -> x <> ' ') stack3
-    (newStack1, newStack2, newStack3)
+let arrangeOneByOne (stacks: Stack<char> list) (instructions: Match list) =
+    let amount = int instructions[0].Value
+    let source = int instructions[1].Value - 1
+    let destination = int instructions[2].Value - 1
+    [1..amount] |> List.iter (fun x -> stacks[destination].Push(stacks[source].Pop()))
 
+let arrangeMultiple (stacks: Stack<char> list) (instructions: Match list) =
+    let amount = int instructions[0].Value
+    let source = int instructions[1].Value - 1
+    let destination = int instructions[2].Value - 1
+    let temp = Stack<char>()
+    [1..amount] |> List.iter (fun x -> temp.Push(stacks[source].Pop()))
+    [1..amount] |> List.iter (fun x -> stacks[destination].Push(temp.Pop()))
+    
 [<EntryPoint>]
 let main args =
     let input = File.ReadLines args[0]
@@ -28,19 +41,28 @@ let main args =
         input
         |> List.ofSeq
         |> splitInput
-    let stacks =
-        stacksInput 
-        |> List.truncate (List.length stacksInput - 1)
-        |> createStacks
-        |> removeEmptyElements
     let instructions =
         instructionsInput
         |> List.filter (fun x -> x <> "")
         |> List.map (fun x -> Regex.Matches(x, "[0-9]+"))
-        |> List.ofSeq
+        |> List.map List.ofSeq
+    let stacks1 =
+        stacksInput 
+        |> List.rev
+        |> createStacks
+    let stacks2 =
+        stacksInput 
+        |> List.rev
+        |> createStacks       
+
+    instructions
+        |> List.iter (arrangeOneByOne stacks1)
+    instructions
+        |> List.iter (arrangeMultiple stacks2)
+     
+    let part1 = stacks1 |> List.map (fun x -> x.Peek())
+    let part2 = stacks2 |> List.map (fun x -> x.Peek())
         
-
-    printfn "%0A" stacks
-    printfn "%0A" instructions
-
+    printfn $"part1: %0A{part1}"
+    printfn $"part1: %0A{part2}"
     0
