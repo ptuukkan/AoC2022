9fe88024ebe2ed7a11d60699cac224ba3af11776
Pekka Tuukkanen <petuukka@gmail.com>
2022-12-06 07:38:42 +0100

    Day 6 part 2

--- day6/Program.fs
+++ day6/Program.fs
@@ -1,10 +1,16 @@
 ﻿open System.IO
 
-let spreadInput (c: char) (acc: string list)=
+let spreadInput (c: char) (acc: string list) =
     match acc.Length with
     | x when x < 1 -> [ string c ]
     | x when x < 4 -> string c + List.head acc  :: acc
     | _ -> string c + (List.head acc).Substring(0, 3) :: acc
+
+let spreadInputPart2 (c: char) (acc: string list) =
+    match acc.Length with
+    | x when x < 1 -> [ string c ]
+    | x when x < 14 -> string c + List.head acc  :: acc
+    | _ -> string c + (List.head acc).Substring(0, 13) :: acc
 
 [<EntryPoint>]
 let main args =
@@ -21,6 +27,17 @@ let main args =
             let distinct = Seq.distinct x |> Seq.length
             distinct = 4)
         |> Operators.(+) 4
+        
+    let part2 =
+        input
+        |> List.head
+        |> List.ofSeq
+        |> fun x -> (x, []) ||> List.foldBack spreadInputPart2
+        |> List.findIndex (fun x ->
+            let distinct = Seq.distinct x |> Seq.length
+            distinct = 14)
+        |> Operators.(+) 14
             
     printfn "%d" part1
+    printfn "%d" part2
     0
