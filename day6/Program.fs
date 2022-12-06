c96cb3d0b078ed957423b23b31b37fcac5e0dfe6
Pekka Tuukkanen <petuukka@gmail.com>
2022-12-06 07:32:15 +0100

    Day 6 part 1

---
+++ day6/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+
+let spreadInput (c: char) (acc: string list)=
+    match acc.Length with
+    | x when x < 1 -> [ string c ]
+    | x when x < 4 -> string c + List.head acc  :: acc
+    | _ -> string c + (List.head acc).Substring(0, 3) :: acc
+
+[<EntryPoint>]
+let main args =
+    let input =
+        File.ReadLines args[0]
+        |> List.ofSeq
+
+    let part1 =
+        input
+        |> List.head
+        |> List.ofSeq
+        |> fun x -> (x, []) ||> List.foldBack spreadInput
+        |> List.findIndex (fun x ->
+            let distinct = Seq.distinct x |> Seq.length
+            distinct = 4)
+        |> Operators.(+) 4
+            
+    printfn "%d" part1
+    0
