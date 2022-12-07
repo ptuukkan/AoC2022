a4deef45726e3dbc0049fa7adc4a6b2af3218e58
Pekka Tuukkanen <petuukka@gmail.com>
2022-12-07 17:49:07 +0100

    Day 7 start

---
+++ day7/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+
+type File = { Name: string; Size: int }
+
+type Directory =
+    { Name: string
+      Directories: Directory list
+      Files: File list }
+
+let executeCommands commands rootDir =
+    rootDir
+    
+
+[<EntryPoint>]
+let main args =
+    let input = File.ReadLines(args[0])
+    let rootDir =
+        { Name = "/"
+          Directories = []
+          Files = [] }
+    rootDir
+    |> executeCommands input
+        
+     
+
+    0
